1a0a6da623f9df40d33bb9e33cf72be1512c5ef6
dvvarma <dvvarma12@gmail.com>
2020-06-08 17:27:46 -0400

    Updates

---
+++ precache-manifest.7cbb8cd8ca3267e51e7c20bce57b50d4.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40007d117e176fa755617c327166bd33",
+    "url": "/Portfolio-using-React/index.html"
+  },
+  {
+    "revision": "6c41ef8ee56fca6ae1c2",
+    "url": "/Portfolio-using-React/static/css/2.1d6d6bff.chunk.css"
+  },
+  {
+    "revision": "b03121d7478b33753f4d",
+    "url": "/Portfolio-using-React/static/css/3.6849462a.chunk.css"
+  },
+  {
+    "revision": "3e2567918bf4936bbaea",
+    "url": "/Portfolio-using-React/static/css/main.2bc65d47.chunk.css"
+  },
+  {
+    "revision": "6c41ef8ee56fca6ae1c2",
+    "url": "/Portfolio-using-React/static/js/2.72b74fcf.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Portfolio-using-React/static/js/2.72b74fcf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b03121d7478b33753f4d",
+    "url": "/Portfolio-using-React/static/js/3.36a54621.chunk.js"
+  },
+  {
+    "revision": "ee5863553a317dae21d2",
+    "url": "/Portfolio-using-React/static/js/4.6bb9adbe.chunk.js"
+  },
+  {
+    "revision": "3e2567918bf4936bbaea",
+    "url": "/Portfolio-using-React/static/js/main.37d385c9.chunk.js"
+  },
+  {
+    "revision": "bebd2a0cc974c04c2810",
+    "url": "/Portfolio-using-React/static/js/runtime-main.672c947d.js"
+  },
+  {
+    "revision": "aa4f4ea2a5548c743c838d77314ee802",
+    "url": "/Portfolio-using-React/static/media/avatar.aa4f4ea2.JPG"
+  },
+  {
+    "revision": "1ab3d2a3bd3c11ff662e557fba0ff37b",
+    "url": "/Portfolio-using-React/static/media/left-back.1ab3d2a3.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/Portfolio-using-React/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/Portfolio-using-React/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/Portfolio-using-React/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/Portfolio-using-React/static/media/slick.f97e3bbf.svg"
+  }
+]);
